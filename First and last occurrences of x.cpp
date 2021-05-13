3a2c36cbaa068f5345b73167d4a85af43fca0c63
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-05-14 00:39:25 +0530

    Create First and last occurrences of x.cpp
    https://practice.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/1#

---
+++ First and last occurrences of x.cpp
@@ -0,0 +1,26 @@
+vector<int> find(int arr[], int n , int x )
+{
+	int j=0;
+	vector<int>answer(2,-1);
+	for(int i=0 ; i<n ; i++)
+	{
+		if(n==0)   
+		return answer;
+    
+		  if(arr[i]==x)
+		  {
+			  answer[0]=i;
+			  j=i;
+			  break;
+		  }
+	}
+
+	for(int i=j ; i<n ; i++) 
+	{
+		  if(arr[i]==x)
+		  {
+		  answer[1]=i;
+		  }
+	}
+return answer;
+}
