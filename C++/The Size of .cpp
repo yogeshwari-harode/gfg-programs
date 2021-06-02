bebde2c24fcc30121672cc2d31b63069ec78551a
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-02 14:42:34 +0530

    Create The Size of .cpp
    https://practice.geeksforgeeks.org/problems/the-size-of/1/?track=module-1-basics-and-control-structures#

---
+++ C++/The Size of .cpp
@@ -0,0 +1,11 @@
+// Function to find size of different data types
+
+
+void dataTypes(int a, float b, double c, long long l, string d){
+	float p = b/c;
+	double q = b/a;
+	int r = c/a;
+	long long m = r + l;
+	cout << sizeof(p) << " " << sizeof(q) << " " << sizeof(r) << " " << sizeof(m) << endl;
+	cout << sizeof(d) << " " << sizeof(d[3]) << endl;
+}
