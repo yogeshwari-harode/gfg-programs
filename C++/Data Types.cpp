ddc6817551068f61ae124f266ac566fee5086e24
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-02 15:31:14 +0530

    Create Data Types.cpp
    https://practice.geeksforgeeks.org/problems/data-types/1/?track=module-1-basics-and-control-structures#

---
+++ C++/Data Types.cpp
@@ -0,0 +1,13 @@
+// Function to check and print data types accordingly
+
+void dataTypes(int a, float b, double c, long long l,string d)
+{
+	float p = b/c ;
+	double q = b/a ;
+  int r = c/a ;
+	long long m = r+l;
+
+	cout << p << " " << q << " " << r <<" "<< m <<endl; 
+	cout <<d<<" " <<d[3]<<endl ;
+  
+}
