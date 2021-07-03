13b72d1ed65f4827bc9b39318f3452bbcc563601
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-07-03 12:23:57 +0530

    Create Two Dimensional World.cpp
    https://practice.geeksforgeeks.org/problems/two-dimensional-world/1/?track=module-3-pointers-and-objects%20&batchId=113

---
+++ C++/Two Dimensional World.cpp
@@ -0,0 +1,12 @@
+void twoDimensional(int *mat[],int N)
+{
+	// Loop to iterate through matrix
+	for(int i = 0;i<N;i++)
+	{
+		for(int j = 0;j<N;j++)
+		{
+			cout<<mat[i][j]<<" ";
+		}
+	cout << endl;
+	}
+}
